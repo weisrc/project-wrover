3863d8fa7c42cb4bfff202abfae200eab1956219
Wei <weihengjiang64@hotmail.com>
2024-03-22 15:14:12 -0400

    canvas camera follow vehicule

--- web/components/map-canvas.tsx
+++ web/components/map-canvas.tsx
@@ -8,105 +8,129 @@ export type MapCanvasProps = HTMLAttributes<HTMLDivElement>;
 
 const angles = [-Math.PI / 2, 0, -Math.PI];
 
-function drawSonarRays(ctx: CanvasRenderingContext2D, center: Vec2, angle: number, sonar: SonarData, last: boolean) {
-    const circleRadius = 3;
-    
-    for (let i = 0; i < angles.length; i++) {
-        const distance = sonar[i] / 58;
-        const ray = Vec2.polar(angles[i] + angle, distance);
-        const end = ray.add(center)
+function drawSonarRays(
+  ctx: CanvasRenderingContext2D,
+  rawCenter: Vec2,
+  angle: number,
+  sonar: SonarData,
+  last: boolean,
+  offset: Vec2
+) {
+  const circleRadius = 3;
+  const center = rawCenter.add(offset);
 
-        if (last) {
-            ctx.beginPath()
-            ctx.strokeStyle = "pink";
-            ctx.moveTo(center.x, center.y);
-            ctx.lineTo(end.x, end.y);
-            ctx.stroke()
+  for (let i = 0; i < angles.length; i++) {
+    const distance = sonar[i] / 58;
+    const point = Vec2.polar(angles[i] + angle, distance).add(center);
 
-            ctx.fillStyle = "white"
-            ctx.fillText(distance.toFixed(1) + "cm", end.x + 20, end.y)
-        }
+    if (last) {
+      ctx.beginPath();
+      ctx.strokeStyle = "pink";
+      ctx.moveTo(center.x, center.y);
+      ctx.lineTo(point.x, point.y);
+      ctx.stroke();
 
-        ctx.beginPath()
-        ctx.ellipse(end.x, end.y, circleRadius, circleRadius, 0, 0, 2 * Math.PI * 2);
-        ctx.fillStyle = last ? "red" : "white"
-        ctx.fill()
+      ctx.fillStyle = "white";
+      ctx.fillText(distance.toFixed(1) + "cm", point.x + 20, point.y);
     }
+
+    ctx.beginPath();
+    ctx.ellipse(
+      point.x,
+      point.y,
+      circleRadius,
+      circleRadius,
+      0,
+      0,
+      2 * Math.PI * 2
+    );
+    ctx.fillStyle = last ? "red" : "white";
+    ctx.fill();
+  }
 }
 
-const data: LocomotionData[] = []
+const data: LocomotionData[] = [];
 
 export function MapCanvas(props: MapCanvasProps) {
-
-    useEffect(() => {
-        function onLocomotion(item: LocomotionData) {
-            if (data.length > 10000) {
-                data.shift();
-            }
-            data.push(item);
-            requestLocomotion();
-        }
-
-        function requestLocomotion() {
-            setTimeout(() => {
-                requestEmitter.emit("locomotion", {});
-            }, 100);
-        }
-
-        requestLocomotion()
-
-        responseEmitter.on("locomotion", onLocomotion)
-
-        return () => {
-            responseEmitter.off("locomotion", onLocomotion)
-        }
-    }, [])
-
-    const radius = 15;
-    const diameter = radius * 2;
-    const delta = 0.043;
-
-    const draw: DrawFunction = (ctx, width, height) => {
-        const offset = new Vec2(width / 2, height / 2);
-        let left = new Vec2(-radius, 0).add(offset);
-        let right = new Vec2(radius, 0).add(offset);
-
-        for (let i = 0; i < data.length; i++) {
-            const item = data[i];
-            const isLast = i === data.length - 1
-            let moved = false;
-            for (const char of item.hall) {
-                moved = true;
-
-                // ctx.beginPath();
-                // ctx.moveTo(left.x, left.y)
-                // ctx.lineTo(right.x, right.y)
-                // ctx.strokeStyle = "white";
-                // ctx.stroke()
-
-                const which = char.toLowerCase();
-                const sign = which === char ? 1 : -1
-
-                if (which === "l") {
-                    const angle = left.subtract(right).direction();
-                    left = Vec2.polar(angle + delta * sign, diameter).add(right);
-                } else if (which === "r") {
-                    const angle = right.subtract(left).direction();
-                    right = Vec2.polar(angle - delta * sign, diameter).add(left);
-                }
-            }
-
-            if (!moved && !isLast) {
-                continue;
-            }
-
-            const angle = right.subtract(left).direction();
-
-            drawSonarRays(ctx, left.between(right), angle, item.sonar, isLast)
-
-
-        }
+  useEffect(() => {
+    function onLocomotion(item: LocomotionData) {
+      if (data.length > 10000) {
+        data.shift();
+      }
+      data.push(item);
+      requestLocomotion();
     }
 
-    return <DrawCanvas draw={draw} {...props} background="black" />
+    function requestLocomotion() {
+      setTimeout(() => {
+        requestEmitter.emit("locomotion", {});
+      }, 100);
+    }
+
+    requestLocomotion();
+
+    responseEmitter.on("locomotion", onLocomotion);
+
+    return () => {
+      responseEmitter.off("locomotion", onLocomotion);
+    };
+  }, []);
+
+  const radius = 15;
+  const diameter = radius * 2;
+  const delta = 0.043;
+
+  let offset: Vec2;
+
+  const draw: DrawFunction = (ctx, width, height) => {
+    let left = new Vec2(-radius, 0);
+    let right = new Vec2(radius, 0);
+    const screenCenter = new Vec2(width / 2, height / 2);
+    offset ??= screenCenter;
+
+    for (let i = 0; i < data.length; i++) {
+      const item = data[i];
+      const isLast = i === data.length - 1;
+      let moved = false;
+      for (const char of item.hall) {
+        moved = true;
+
+        // ctx.beginPath();
+        // ctx.moveTo(left.x, left.y)
+        // ctx.lineTo(right.x, right.y)
+        // ctx.strokeStyle = "white";
+        // ctx.stroke()
+
+        const which = char.toLowerCase();
+        const sign = which === char ? 1 : -1;
+
+        if (which === "l") {
+          const angle = left.subtract(right).direction();
+          left = Vec2.polar(angle + delta * sign, diameter).add(right);
+        } else if (which === "r") {
+          const angle = right.subtract(left).direction();
+          right = Vec2.polar(angle - delta * sign, diameter).add(left);
+        }
+      }
+
+      if (!moved && !isLast) {
+        continue;
+      }
+
+      const angle = right.subtract(left).direction();
+
+      drawSonarRays(
+        ctx,
+        left.between(right),
+        angle,
+        item.sonar,
+        isLast,
+        offset
+      );
+    }
+
+    offset = screenCenter.subtract(left.between(right));
+  };
+
+  return <DrawCanvas draw={draw} {...props} background="black" />;
 }
