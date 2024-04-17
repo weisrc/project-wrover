b0e67448284b46662eb98987d54093c9a1d3f412
schoolwei <weihengjiang255@gmail.com>
2024-04-17 15:47:58 -0400

    add 3d rover

--- web/components/canvas/map-canvas-3d.tsx
+++ web/components/canvas/map-canvas-3d.tsx
@@ -1,19 +1,56 @@
 import { Canvas, useFrame } from "@react-three/fiber"
-import { PerspectiveCamera } from "@react-three/drei"
+import { Line, OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import * as THREE from "three";
+import { useState } from "react";
+import { LineGeometry } from "three/examples/jsm/Addons.js";
 
-export function ThreeRover() {
-    return <mesh >
+type RoverProps = {
+    position: THREE.Vector3
+    rotation: THREE.Euler
+}
+
+export function Rover({ position, rotation }: RoverProps) {
+    return <mesh position={position} rotation={rotation}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color='orange' />
+        <Line
+            points={[
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(0, 0, -10)
+            ]}
+            lineWidth={2}
+            dashed
+        />
+        <Line
+            points={[
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(10, 0, 0)
+            ]}
+            lineWidth={2}
+            dashed
+        />
+        <Line
+            points={[
+                new THREE.Vector3(0, 0, 0),
+                new THREE.Vector3(-10, 0, 0)
+            ]}
+            lineWidth={2}
+            dashed
+        />
     </mesh>
 }
 
 export function MapCanvas3D() {
+
+    const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0));
+    const [rotation, setRotation] = useState(new THREE.Euler())
+
+
     return <>
         <Canvas>
             <ambientLight intensity={Math.PI / 2} />
-            <ThreeRover />
-            <PerspectiveCamera makeDefault manual />
+            <Rover position={position} rotation={rotation} />
+            <OrbitControls target={position} />
         </Canvas>
     </>
 }
